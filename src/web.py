2169fe95e452adb6e560d5667471053b9af00706
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2024-04-26 09:14:50 +0200

    initial commit

---
+++ src/web.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from datetime import datetime
+import time
+
+time.sleep(10)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    now = datetime.now()
+    day = now.strftime("%d")
+    month = now.strftime("%m")
+    year = now.strftime("%y")
+    con = open(f"logs/{day}_{month}_{year}log.txt", encoding="UTF-8").readlines()
+    status = open("status.txt", "r").read()
+    text = f"<p>{status}</p>"
+    for i in con:
+        text += "<p>" + i + "</p>"
+    return text
+
+app.run(host="194.164.48.116", port="5000")
