bd5a612bfa92e31010c991e53d44777414a33c30
Michael <michael.martynov03@gmail.com>
2024-05-06 23:37:24 +0200

    add datetime and write into logs

--- src/api_wrapper.py
+++ src/api_wrapper.py
@@ -3,6 +3,7 @@ import deutsche_bahn_api.station_helper
 import deutsche_bahn_api.timetable_helper
 import config
 import mysql.connector
+import datetime
 
 class Api_wrapper:
     
@@ -88,5 +89,8 @@ class Api_wrapper:
                 mydb.commit()
 
         mydb.close()
-        print("Success--End of script")
+
+        print("End of script "+str(datetime.datetime.now()))
+        with open("logs/logs.txt",'a') as datei:
+            datei.write("End of script "+str(datetime.datetime.now())+"\n")
 
