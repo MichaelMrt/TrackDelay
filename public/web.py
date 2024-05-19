35f771d5857bc28083f52cb829e73cb91d2fc4a3
Michael <michael.martynov03@gmail.com>
2024-05-19 20:21:44 +0200

    add cancel probability, trainstation list and RB50 line infos to webpage

--- public/web.py
+++ public/web.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import os
 import mysql.connector
 import sys
+import re
 
 # jump up one folder to import the config file
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -31,9 +32,40 @@ def index():
     mycursor.execute(query)
     results = mycursor.fetchall()
     verspaetung_in_min = results[0][0]
-    print(results[0][0])
 
-    return render_template('index.html',anzahl_zuege=anzahl_zuege,verspaetung_in_min=verspaetung_in_min)
+    # Durchschnittliche Ausfallwahrscheinlichkeit in %
+    query = "SELECT ((SELECT COUNT(*) FROM trains WHERE current_departure IS NULL) /(SELECT COUNT(*) FROM trains))*100"
+    mycursor.execute(query)
+    results = mycursor.fetchall()
+    ausfallwahrscheinlichkeit = results[0][0]
+
+    # Bahnhöfe Liste
+    query = "SELECT DISTINCT train_station FROM trains"
+    mycursor.execute(query)
+    results = mycursor.fetchall()
+    bahnhoefe=""
+    for bahnhof in results:
+     bahnhoefe += str(bahnhof)
+    # Zeichen entferenn
+    bahnhoefe = re.sub('[(\']',"",bahnhoefe)
+    bahnhoefe = re.sub('[)]'," ",bahnhoefe)
+
+    # RB50 Daten
+    # Anzahl RB50
+    query = "SELECT COUNT(*) FROM trains WHERE line ='RB50'"
+    mycursor.execute(query)
+    results = mycursor.fetchall()
+    rb50_anzahl = results[0][0]
+    # Ausfallwahrscheinlichkeit RB50
+    query = "SELECT ((SELECT COUNT(*) FROM trains WHERE current_departure IS NULL AND line='RB50') /(SELECT COUNT(*) FROM trains WHERE line='RB50'))*100"
+    mycursor.execute(query)
+    results = mycursor.fetchall()
+    rb50_ausfallwahrscheinlichkeit = results[0][0]
+
+
+
+    return render_template('index.html',anzahl_zuege=anzahl_zuege,verspaetung_in_min=verspaetung_in_min,
+                           ausfallwahrscheinlichkeit=ausfallwahrscheinlichkeit, bahnhoefe=bahnhoefe,rb50_anzahl=rb50_anzahl,rb50_ausfallwahrscheinlichkeit=rb50_ausfallwahrscheinlichkeit)
 
 
 if __name__ == '__main__':
