a2efb18da9e4aa557ff3ad5cc3a0b582c0358d66
Michael <michael.martynov03@gmail.com>
2024-11-29 16:00:11 +0100

    Removed loop

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-import sched
-import time
 import api_wrapper
 import os
 import traceback
@@ -13,32 +11,23 @@ error_log_path = os.path.join(log_path, "error.log")
 if not os.path.exists(log_path):
    os.makedirs(log_path)
 
-def repeat_task():
-    try:
-     api_wrapper.start("Bonn")
-     api_wrapper.start("Köln")
-     api_wrapper.start("Münster")
-     api_wrapper.start("Düsseldorf Hbf")
-     api_wrapper.start("Duisburg")
-     api_wrapper.start("Essen")
-     api_wrapper.start("Wuppertal")
-     api_wrapper.start("Bochum")
-     api_wrapper.start("Berlin Hbf")
-     api_wrapper.start("Lünen")
-     api_wrapper.start("Ennepetal")
-     
-    except Exception as e:
-       with open(error_log_path,'a') as error_log:
-          print("ERROR")
-          error_log.write("ERROR:\n")
-          traceback.print_exc(file=error_log)
-
-    
-    scheduler.enter(30, 1, repeat_task)
-
 
 api_wrapper = api_wrapper.Api_wrapper()
 
-scheduler = sched.scheduler(time.time, time.sleep)
-repeat_task()
-scheduler.run()
+try:
+   api_wrapper.start("Bonn")
+   api_wrapper.start("Köln")
+   api_wrapper.start("Münster")
+   api_wrapper.start("Düsseldorf Hbf")
+   api_wrapper.start("Duisburg")
+   api_wrapper.start("Essen")
+   api_wrapper.start("Wuppertal")
+   api_wrapper.start("Bochum")
+   api_wrapper.start("Berlin Hbf")
+   api_wrapper.start("Lünen")
+   api_wrapper.start("Ennepetal") 
+except Exception as e:
+   with open(error_log_path,'a') as error_log:
+          print("ERROR")
+          error_log.write("ERROR:\n")
+          traceback.print_exc(file=error_log)
