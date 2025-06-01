ecfc67daaee545e909872b9fe38d53277881909a
Michael <michael.martynov03@gmail.com>
2025-06-01 13:54:31 +0200

    Turn TrainData class to a dataclass

--- src/train_data.py
+++ src/train_data.py
@@ -1,11 +1,13 @@
-class TrainData():
-    def __init__(self, line, train_id, first_station, last_station, planned_departure, current_departure, track, message, train_station):
-        self.line = line
-        self.train_id = train_id
-        self.first_station = first_station
-        self.last_station = last_station
-        self.planned_departure = planned_departure
-        self.current_departure = current_departure
-        self.track = track
-        self.message = message
-        self.train_station = train_station
+from dataclasses import dataclass
+
+@dataclass
+class TrainData:
+    line: str
+    train_id: str
+    first_station: str
+    last_station: str
+    planned_departure: str
+    current_departure: str
+    track: str
+    message: str
+    train_station: str
